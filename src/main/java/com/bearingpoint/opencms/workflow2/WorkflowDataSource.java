9e789479921202b6a38d8878419117eb907ebdb0
David Trattnig <david.trattnig@gmail.com>
2008-01-17 16:01:30 +0000

    added default usage for opencms database/datasource configured inside opencms.properties
    git-svn-id: https://bp-cms-commons.svn.sourceforge.net/svnroot/bp-cms-commons/opencms_workflow2/trunk@465 ad7af0c5-3b20-0410-96b2-8d8c450db5ee

---
+++ src/main/java/com/bearingpoint/opencms/workflow2/WorkflowDataSource.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.bearingpoint.opencms.workflow2;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.opencms.configuration.CmsConfigurationManager;
+import org.opencms.main.CmsLog;
+
+/**
+ * WorkflowDataSource
+ * <p>
+ * This class holds the database configuration for the workflow tables.
+ * By default these tables are stored within the current OpenCms database.
+ * The properties are read from the opencms.properties file.
+ * <p>
+ * @author David.trattnig
+ *
+ */
+public class WorkflowDataSource extends org.springframework.jdbc.datasource.DriverManagerDataSource {
+
+	private static Map data;
+	private static final Log LOG = CmsLog.getLog(WorkflowDataSource.class);
+	
+	public WorkflowDataSource() {
+
+		if (data != null) {
+			//TODO check if URL parameters is the same as connection properties??
+			setUrl((String) data.get("db.pool.default.jdbcUrl")+(String) data.get("db.pool.default.jdbcUrl.params"));
+			setUsername((String) data.get("db.pool.default.user"));
+			setPassword((String) data.get("db.pool.default.password"));
+			setDriverClassName((String) data.get("db.pool.default.jdbcDriver"));
+			LOG.info("WORKFLOW2 DATABASE: properties assigned to hibernate!");
+		}
+		else {
+			/* the workflow module hasn't been initialized yet, but now worries, the springmanager will retry. */
+			LOG.info("WORKFLOW2 DATABASE: hibernate init in the first round hasn't been successfull. hope there is another one...");
+		}
+	}
+	
+	public static void initDatasourceData(CmsConfigurationManager config) {
+		
+		data = config.getConfiguration();
+		LOG.info("WORKFLOW2 DATABASE: configuration initialized!");
+	}
+}
